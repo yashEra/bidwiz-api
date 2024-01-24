1ed67725a04513d65f9150f4e63e2a7555f66360
Yasith Eranda Yapa Amarasekara <106188847+yashEra@users.noreply.github.com>
2024-01-24 15:17:20 +0530

    Error Fixed

--- bwiz_api/users/views.py
+++ bwiz_api/users/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serailizers import StandardSerializer
+from .serializers import StandardSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
 
 # Create your views here.
 
+def post(request, *args, **kwargs):
+    print(request.data)
+    serializer = StandardSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"data": serializer.data})
+    else:
+        return Response({'data': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class StandardAPIViews(APIView):
-    def post(self, request, *args, **kwargs):
-        print(request.data)
-        serializer = StandardSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"data": serializer.data})
-        else:
-            return Response({'data': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    pass
 
 
 
