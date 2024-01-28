c249c06dc8bf4a9ec8f1d5bd58ba5a9eeb6914ce
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2024-01-28 10:16:45 +0530

    user logout

--- bwiz_api/bwiz_api/settings.py
+++ bwiz_api/bwiz_api/settings.py
@@ -137,6 +137,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':('knox.auth.TokenAuthentication',),
 }
